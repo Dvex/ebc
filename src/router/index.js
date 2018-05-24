32005fcd81f390ea185cd713b9c57e6df6425c07
Eduardo Cabrera Japa <dvex_92@hotmail.com>
2018-05-23 23:54:41 -0500

    Set home page in routes

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,11 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
+      path: '/',
+      name: 'QuienesSomos',
+      component: QuienesSomos
+    },
+    {
       path: '/quienes.index',
       name: 'QuienesSomos',
       component: QuienesSomos
