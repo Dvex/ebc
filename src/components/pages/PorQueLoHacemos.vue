7754a9ac0e39caaac55a96f8c3254d17adae3360
Gerson Perez Aguilar <gperezdesign@gmail.com>
2018-05-29 02:35:12 -0500

    avance al 20%

--- src/components/pages/PorQueLoHacemos.vue
+++ src/components/pages/PorQueLoHacemos.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>
+  <div class="prblockA">
     <v-flex xs12 md12 style="text-align:center">
       <h1>POR QUÉ LO HACEMOS</h1>
     </v-flex>
-
-    <v-flex xs12 md12>
+    <div>
+    <img src="./assets/img1-porque.png">
+    </div> 
+    <v-flex xs12 md12> 
       <p>
       No hay momento más maravilloso que ese en que Dios toma nuestro corazón y nuestra mente y nos invita a iniciar este viaje de descubrimiento. Las grandes preguntas de ¿quienes somos? ¿por qué estamos aquí? Ya tienen respuesta, porque “la palabra de Dios es viva y eficaz”. Nuestra experiencia da como resultado que “crecemos en conocer y amar más a Dios”.
       </p>
@@ -17,6 +19,31 @@
       </p>
     </v-flex>
   </div>
+  <div class="prblockB">
+    <figure> 
+      <img src="./assets/img2-porque.png">
+    </figure>
+    <h3>CONTACTANOS</h3>
+    <p>Estaremos encantados de escucharte. Contáctate con nosotros a través de los siguientes medios:</p>
+    <div class="list-hacemos">
+      <div class="item-hacemos">
+        <strong>POR CORREO
+        <a href="mailto:info@ebc.org.pe" target="_blank">info@ebc.org.pe</a>
+      </div>
+      <div class="item-hacemos">
+        <strong>EN FACEBOOK
+        <a href="www.facebook.com/ebcperu" target="_blank">www.facebook.com/ebcperu</a>
+      </div>
+      <div class="item-hacemos">
+        <strong>POR TÉLEFONO
+        <a href="tel:(51) 999 999 999" target="_blank">(51) 999 999 999</a>
+        <a href="tel:(51) 999 999 999" target="_blank">(51) 999 999 999</a>
+      </div>
+      <div class="item-hacemos">
+        <strong>EN YOUTUBE
+        <a href="www.youtube.com/ebcperu" target="_blank">www.youtube.com/ebcperu</a>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -28,3 +55,5 @@ export default {
   }
 }
 </script>
+
+
